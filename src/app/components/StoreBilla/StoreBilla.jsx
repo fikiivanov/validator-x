bf6ab70e3d40f6761a68e1fad320a0875ff87749
Piko327 <fikii.business@gmail.com>
2024-07-13 13:49:19 +0300

    Update StoreBilla.jsx

--- src/app/components/StoreBilla/StoreBilla.jsx
+++ src/app/components/StoreBilla/StoreBilla.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { FaStoreAlt } from "react-icons/fa";
 import './StoreBilla.scss';
@@ -5,12 +6,14 @@ import './StoreBilla.scss';
 const StoreBilla = ({ currentStore, setStores }) => {
     const [checked, setChecked] = useState(() => {
         // Try to get the 'stores' data from local storage
-        const storedStores = localStorage.getItem('stores');
-        if (storedStores) {
-            // Parse the stored data and find if the current store is checked
-            const parsedStores = JSON.parse(storedStores);
-            const foundStore = parsedStores.find(store => store.number === currentStore.number);
-            return foundStore ? foundStore.checked : false;
+        if (typeof window !== 'undefined') {
+            const storedStores = localStorage.getItem('stores');
+            if (storedStores) {
+                // Parse the stored data and find if the current store is checked
+                const parsedStores = JSON.parse(storedStores);
+                const foundStore = parsedStores.find(store => store.number === currentStore.number);
+                return foundStore ? foundStore.checked : false;
+            }
         }
         // If no stored data or the current store is not found, default to false
         return false;
@@ -22,27 +25,28 @@ const StoreBilla = ({ currentStore, setStores }) => {
     };
 
     useEffect(() => {
-        // Update local storage whenever the checked state changes
-        const storedStores = localStorage.getItem('stores');
-        let parsedStores = [];
-        let currentStoreNum=currentStore.number
-        if (storedStores) {
-            parsedStores = JSON.parse(storedStores);
+        if (typeof window !== 'undefined') {
+            // Update local storage whenever the checked state changes
+            const storedStores = localStorage.getItem('stores');
+            let parsedStores = [];
+            if (storedStores) {
+                parsedStores = JSON.parse(storedStores);
+            }
+            // Find the index of the current store in the parsed stores array
+            const index = parsedStores.findIndex(store => store.number === currentStore.number);
+            if (index !== -1) {
+                // Update the checked property of the current store in the parsed stores array
+                parsedStores[index].checked = checked;
+            } else {
+                // If the current store is not found, add it to the parsed stores array
+                parsedStores.push({ number: currentStore.number, checked });
+            }
+            // Save the updated stores array to local storage
+            localStorage.setItem('stores', JSON.stringify(parsedStores));
+            // Update the state in the parent component
+            setStores(parsedStores);
         }
-        // Find the index of the current store in the parsed stores array
-        const index = parsedStores.findIndex(store => store.number === currentStoreNum);
-        if (index !== -1) {
-            // Update the checked property of the current store in the parsed stores array
-            parsedStores[index].checked = checked;
-        } else {
-            // If the current store is not found, add it to the parsed stores array
-            parsedStores.push({ number: currentStore.number, checked });
-        }
-        // Save the updated stores array to local storage
-        localStorage.setItem('stores', JSON.stringify(parsedStores));
-        // Update the state in the parent component
-        setStores(parsedStores);
-    }, [checked]);
+    }, [checked, currentStore.number, setStores]);
 
     return (
         <div className="store-billa">
