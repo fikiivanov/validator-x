3199bfb7fb55ebc01e01836430d74f2d6035f0ef
Fiki ivanov <fikii.business@gmail.com>
2024-12-01 19:42:48 +0200

    Sort stores before rendering and Update page.jsx

--- src/app/billa/page.jsx
+++ src/app/billa/page.jsx
@@ -5,33 +5,43 @@ import { firstStores } from '../../../public/stores'
 import StoreBilla from '../components/StoreBilla/StoreBilla'
 import './Billa.scss'
 import Menu from '../components/Menu/Menu'
+
 const Page = () => {
     const [stores, setStores] = useState(firstStores);
 
-    return (<>
-        <Header />
+    // Sort stores by number before rendering
+    const sortedStores = [...stores].sort((a, b) => a.number - b.number);
 
-        <div className="shell">
-            <div className='first-stores'>
-                <div className="first-stores__inner">
-                    <h1>Билла</h1>
+    return (
+        <>
+            <Header />
 
-                    <div className='wrapper'>
-                        <div className='stores'>
-                            {stores.map((store) => <StoreBilla key={store.id}
-                                
-                            stores={stores} currentStore={store} setStores={setStores} />)}
+            <div className="shell">
+                <div className='first-stores'>
+                    <div className="first-stores__inner">
+                        <h1>Билла</h1>
+
+                        <div className='wrapper'>
+                            <div className='stores'>
+                                {sortedStores.map((store) => (
+                                    <StoreBilla 
+                                        key={store.id}
+                                        stores={stores}
+                                        currentStore={store}
+                                        setStores={setStores} 
+                                    />
+                                ))}
+                            </div>
+
+                            <Menu stores={stores} setStores={setStores} />
+
                         </div>
-
-                        <Menu stores={stores} setStores={setStores} />
-
                     </div>
+
                 </div>
-
             </div>
-        </div>
-    </>);
-
+        </>
+    );
 }
 
 export default Page;
